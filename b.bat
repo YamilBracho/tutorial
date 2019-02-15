72046820f9879cc2748f9a91c3d6c5f9c9f0f8c2
YamilBracho <YamilBracho@outlook.com>
2019-02-15 18:09:37 -0500

    Tutorial App

---
+++ b.bat
@@ -0,0 +1,8 @@
+git init
+git add .
+git commit -m "Tutorial App"
+git push -u --force origin master
+
+rem git init
+rem ng build --prod --base-href https://yamilbracho.github.io/tutorial/ 
+rem git add -f ./dist
